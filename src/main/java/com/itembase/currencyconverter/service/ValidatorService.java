0479ec0766c67ee7f163fea1d673315bae4fef8d
alidemirsoy <alidemirsoy@gmail.com>
2021-06-22 13:47:03 +0300

    added length validation for request fields

--- src/main/java/com/itembase/currencyconverter/service/ValidatorService.java
+++ src/main/java/com/itembase/currencyconverter/service/ValidatorService.java
@@ -10,8 +10,12 @@ public class ValidatorService {
     public void validateConversionRequest(ConversionRequest request) {
         if (request.getFrom() == null) {
             throw new InvalidConversionRequestException("'from' field is mandatory!");
+        } else if (request.getFrom().length() != 3) {
+            throw new InvalidConversionRequestException("'from' field should be a 3 letter currency code");
         } else if (request.getTo() == null) {
             throw new InvalidConversionRequestException("'to' field is mandatory!");
+        } else if (request.getTo().length() != 3) {
+            throw new InvalidConversionRequestException("'to' field should be a 3 letter currency code");
         } else if (request.getAmount() == null) {
             throw new InvalidConversionRequestException("'amount' field is mandatory!");
         }
--- src/test/java/com/itembase/currencyconverter/service/ValidatorServiceTest.java
+++ src/test/java/com/itembase/currencyconverter/service/ValidatorServiceTest.java
@@ -9,14 +9,26 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class ValidatorServiceTest {
 
     @InjectMocks
     ValidatorService validatorService;
+
+    @Test
+    void validateConversionRequest_shouldValidateSuccessfulRequest() {
+
+        //Given
+        ConversionRequest request = new ConversionRequest();
+        request.setFrom("EUR");
+        request.setTo("USD");
+        request.setAmount(new BigDecimal("10"));
+
+        //When
+        assertDoesNotThrow(() -> validatorService.validateConversionRequest(request));
+    }
 
     @Test
     void validateConversionRequest_shouldValidateFromField() {
@@ -29,7 +41,24 @@ class ValidatorServiceTest {
         //When
         Exception exception = assertThrows(InvalidConversionRequestException.class, () ->
                 validatorService.validateConversionRequest(request));
+        //Then
         assertEquals("'from' field is mandatory!", exception.getMessage());
+    }
+
+    @Test
+    void validateConversionRequest_shouldValidateFromFieldLength() {
+
+        //Given
+        ConversionRequest request = new ConversionRequest();
+        request.setFrom("ABCD");
+        request.setTo("USD");
+        request.setAmount(new BigDecimal("10"));
+
+        //When
+        Exception exception = assertThrows(InvalidConversionRequestException.class, () ->
+                validatorService.validateConversionRequest(request));
+        //Then
+        assertEquals("'from' field should be a 3 letter currency code", exception.getMessage());
     }
 
     @Test
@@ -43,7 +72,24 @@ class ValidatorServiceTest {
         //When
         Exception exception = assertThrows(InvalidConversionRequestException.class, () ->
                 validatorService.validateConversionRequest(request));
+        //Then
         assertEquals("'to' field is mandatory!", exception.getMessage());
+    }
+
+    @Test
+    void validateConversionRequest_shouldValidateToFieldLength() {
+
+        //Given
+        ConversionRequest request = new ConversionRequest();
+        request.setFrom("USD");
+        request.setTo("AB");
+        request.setAmount(new BigDecimal("10"));
+
+        //When
+        Exception exception = assertThrows(InvalidConversionRequestException.class, () ->
+                validatorService.validateConversionRequest(request));
+        //Then
+        assertEquals("'to' field should be a 3 letter currency code", exception.getMessage());
     }
 
     @Test
@@ -57,6 +103,7 @@ class ValidatorServiceTest {
         //When
         Exception exception = assertThrows(InvalidConversionRequestException.class, () ->
                 validatorService.validateConversionRequest(request));
+        //Then
         assertEquals("'amount' field is mandatory!", exception.getMessage());
     }
 }
